3a3218a6f3f742d5a852e924131bc28ab62918c0
Stevennesski <lvelascomaxino3@gmail.com>
2026-02-06 01:48:11 +0800

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,15 @@
+const paswordInput =
+document.getElementById("password");
+const result = document.getElementById("result")
+
+passwordInput.addEventListemer("input", () => {
+  const password = passwordInput.value;
+
+  lf (password.length <9) {
+    result.textContent = "Strength: Weak";
+  } else if ("password.length < 11 =) {
+    result.textContent = "Stength: Medium";
+} else {
+  result.textContent = "Strength: Strong";
+}
+});
