ceae034f7b6a4cc6f3e0d18e6c84a1ba71c61ec6
Stevennesski <lvelascomaxino3@gmail.com>
2026-02-06 02:32:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const paswordInput =
 document.getElementById("password");
 const result = document.getElementById("result")
 
-passwordInput.addEventListemer("input", () => {
+passwordInput.addEventListener("input", () => {
   const password = passwordInput.value;
 
   lf (password.length <9) {
     result.textContent = "Strength: Weak";
   } else if ("password.length < 11 =) {
     result.textContent = "Stength: Medium";
-} else {
+  } else {
   result.textContent = "Strength: Strong";
-}
+  }
 });
